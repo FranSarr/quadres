02338bd6c0a7489ddcf8bdd26f2d7ff4d9e74446
fsarrias <fsarrias@kolab.es>
2020-06-19 15:38:10 +0200

    No consigo que el POST tome name como parametro

--- src/main/java/com/example/quadres/application/ShopController.java
+++ src/main/java/com/example/quadres/application/ShopController.java
@@ -4,9 +4,7 @@ import com.example.quadres.domain.Shop;
 import com.example.quadres.persistence.HelperShopRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller // This means that this class is a Controller
 @RequestMapping //(path="/shops") This means URL's start with /shops (after Application path)
@@ -20,5 +18,19 @@ public class ShopController {
     Iterable<Shop> getAllShops() {
         // This returns a JSON or XML with all the shops
         return helperShopRepository.findAll();
+    }
+
+    @PostMapping(path="/shops")
+    public @ResponseBody String addNewShop (@RequestParam String name, @RequestParam Integer capacity) {
+        // @ResponseBody means the returned String is the response, not a view name
+        // @RequestParam means it is a parameter from the GET or POST request
+        Shop newShop = new Shop();
+        newShop.setName(name);
+        newShop.setCapacity(capacity);
+        helperShopRepository.save(newShop);
+        return "New Shop created";
+
+        //public Shop newShop(@RequestBody Shop newShop) {
+        //return helperShopRepository.save(newShop);
     }
 }
--- src/main/java/com/example/quadres/persistence/HelperShopRepository.java
+++ src/main/java/com/example/quadres/persistence/HelperShopRepository.java
@@ -1,9 +1,9 @@
 package com.example.quadres.persistence;
 
 import com.example.quadres.domain.Shop;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 public interface HelperShopRepository extends CrudRepository<Shop, Integer> {
-
     public Shop findByName(String name);
 }
